4b4d09a7bb19b33becaf2c547a3000da1108f19a
HYEJINLEE <crdrst777@gmail.com>
2023-02-28 15:38:53 +0900

    feat: 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+import ToDoList from "./ToDoList";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Source Sans Pro', serif;
+  background-color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
+}
+a {
+  text-decoration: none;
+  color: inherit;
+  &:link,&:visited{
+  color: inherit;
+  }
+}
+button{
+  cursor: pointer;
+  border: none;
+  padding: 0;
+}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <ToDoList />
+    </>
   );
 }
-
 export default App;
